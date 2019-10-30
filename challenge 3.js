3f3f07760cdf8fdabf1ad788010723f31d61c24f
fayaad98 <57195747+fayaad98@users.noreply.github.com>
2019-10-30 21:55:55 +0200

    Delete challenge 3.js

--- challenge 3.js
+++
@@ -1,8 +0,0 @@
-// Time to solve 4:50
-
-var num = prompt("Enter a number to multiply");
-var multiplicationAmount = prompt("How many multiples would you like ?",12);
-
-for(var i = 0; i <= multiplicationAmount; i++){
-	console.log(num+" X "+i+" = "+ num*i);
-}

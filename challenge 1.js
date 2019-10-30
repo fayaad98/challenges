9ceb8994e2cd94716f0876ddd8da718522644ccd
fayaad98 <57195747+fayaad98@users.noreply.github.com>
2019-10-30 22:30:10 +0200

    Update challenge 1.js

--- challenge 1.js
+++ challenge 1.js
@@ -6,7 +6,7 @@ for (var i = 1; i <= 100; i++) { // for loop until 100 starting from 1
   }else if(i%3==0){ //else if 3 can go into variable i without any remainders then print fizz
   	console.log("fizz");
   }else if(i%5==0){
-  	console.log("buzz"); // else if 3 can go into variable i without any remainders then print fizz
+  	console.log("buzz"); // else if 5 can go into variable i without any remainders then print buzz
   }else{
   console.log(i); //else print the variable i
   }
